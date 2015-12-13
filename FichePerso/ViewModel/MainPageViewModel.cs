e9f548dec1553be7ad26220101188040e30f9f70
Thomas D'Hulst <thomas.dhulst@gmail.com>
2015-12-13 22:30:59 +0100

    Init

---
+++ FichePerso/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichePerso.ViewModel
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private ObservableCollection<string> _testItems;
+        public ObservableCollection<string> TestItems
+        {
+            get { return _testItems; }
+            set
+            {
+                _testItems = value;
+                RaisePropertyChanged("TestItems");
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            List<string> test = new List<string>();
+            test.Add("coucou");
+            test.Add("test");
+            test.Add("coucou");
+            test.Add("test");
+            test.Add("coucou");
+            test.Add("test");
+            test.Add("coucou");
+            test.Add("test");
+            TestItems = new ObservableCollection<string>(test);
+        }
+
+    }
+}
