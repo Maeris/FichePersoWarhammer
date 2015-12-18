4ad2434363a0683973f4052a6a951a6b0bc9762b
unknown <thomas.dhulst.epitech@gmail.com>
2015-12-18 19:23:59 +0100

    ajout de la vue création de personnage + suppression de fichiers inutiles

---
+++ FichePerso/ViewModel/CharacterCreationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FichePerso.ViewModel
+{
+    public class CharacterCreationViewModel : ViewModelBase
+    {
+        #region Attributes
+
+        public INavigationService _navigationService { get; set; }
+
+        #endregion
+
+        public CharacterCreationViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
